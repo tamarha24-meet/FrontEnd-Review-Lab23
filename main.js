0fddca2d86449894ec05927ab1b63f37aa76548f
tamarha24-meet <139680727+tamarha24-meet@users.noreply.github.com>
2023-07-17 14:14:14 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-function changeBackgroundColor(color){
+/* function changeBackgroundColor(color){
 	document.body.style.backgroundColor = "powderblue";
+} */
+
+function changeBackgroundColor(color){
+	var color = document.body.style.backgroundColor;
+ if(color == "powderblue"){
+ 	document.body.style.backgroundColor = "rgba(175, 225, 175)";
+ }
+ else{
+ 	document.body.style.backgroundColor = "powderblue";
+ }
 }
